091d1834de54c370154229ba5b9b1a02332510cb
Eng.Almo <almobda2012@gmail.com>
2019-02-14 15:40:50 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,7 +21,8 @@ cardImage: "images/king-of-diamonds.png"
 }
 ];
 var cardsInPlay = [];
-var checkForMatch = function(){
+var checkForMatch = function(img){
+  
   if (cardsInPlay.length === 2) {
   if (cardsInPlay[0] === cardsInPlay[1]){
     alert("You found a match!");
@@ -30,12 +31,22 @@ var checkForMatch = function(){
   }
   }
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+}
+}
+createBoard();
